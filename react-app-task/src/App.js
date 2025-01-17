d8dd18304e70603416217ab33ba9bd670ae8d2c1
MAXIM RUBTSOV <some1.92@yandex.ru>
2025-01-17 23:51:02 +0300

    react app.js changed

--- react-app-task/src/App.js
+++ react-app-task/src/App.js
@@ -2,26 +2,40 @@ import logo from "./logo.svg";
 import "./App.css";
 import moment from "moment";
 import "moment/locale/ru";
+import { createElement } from "react";
 
 function App() {
 	moment.locale("ru");
 	const date = moment(new Date()).format("LL");
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Сейчас {date}</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+
+	const reactLogo = createElement("img", {
+		className: "App-logo",
+		src: logo,
+		alt: "logo",
+	});
+
+	const paragraph = createElement("p", null, `Сейчас ${date}`);
+
+	const link = createElement(
+		"a",
+		{
+			className: "App-link",
+			href: "https://reactjs.org",
+			target: "_blank",
+			rel: "noopener noreferrer",
+		},
+		"Learn React"
 	);
+
+	const appHeader = createElement(
+		"header",
+		{ className: "App-header" },
+		reactLogo,
+		paragraph,
+		link
+	);
+
+	return createElement("div", { className: "App" }, appHeader);
 }
 
 export default App;
